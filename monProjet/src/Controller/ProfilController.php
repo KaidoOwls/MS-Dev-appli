503256ad8cf2b958a5765d51d396728ffbb49d6f
KaidoOwls <seydina.diallo60@gmail.com>
2023-08-23 16:53:44 +0200

    modif

--- monProjet/src/Controller/ProfilController.php
+++ monProjet/src/Controller/ProfilController.php
@@ -6,10 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-
-
-
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
@@ -22,6 +18,4 @@ class ProfilController extends AbstractController
         ]);
     }
 }
-
-
 
